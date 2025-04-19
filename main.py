eeabe8925294df64abef5a5a5fb4f34763c7be68
David Vaz <74098173+davidvaz1@users.noreply.github.com>
2025-04-19 12:03:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ data = pandas.read_csv("birthdays.csv")
 birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
 if today_tuple in birthdays_dict:
     birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_{random.randint(4,10)}.txt"
+    file_path = f"letter_{random.randint(1, 5)}.txt"
     with open(file_path) as letter_file:
         contents = letter_file.read()
         contents = contents.replace("[NAME]", birthday_person["name"])
